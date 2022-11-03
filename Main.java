b50641df2e13c98838cceb0e7a5ed01f173f1c2a
adil1567 <adil.zhiyenbayev@nu.edu.kz>
2022-11-04 01:33:42 +0300

    The second homework

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //The first code
+        System.out.println("Привет, мир! Я изучаю Java.");
+        /*Testing
+        multiple comments
+         */
+    }
+}
